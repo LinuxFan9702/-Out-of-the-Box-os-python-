f48f2fd6601389deb5f7cb1f52fc3b22215bd921
Linux Fan 9700 <cartermay0w0@gmail.com>
2026-07-24 12:51:21 -0400

    Add files via upload

---
+++ OutOfTheBoxOS.py
@@ -0,0 +1,20 @@
+                                # This is ware Out of the Box Starts!
+ # Notes, this is somthing that contains Code form AI, not saying its fully coded by AI, Ai was only used for helping with #finding imports for features and also help with finding bugs in code. so its Some what Vibe Coded, not fully!
+
+#Imports!
+
+import time
+import os
+import math
+
+# This is ware are code starts!
+
+print("Welcome to my Oprating system Shood boot any second now!")
+print("Also No this is not a custom kernel (in a way) but no real os to boot into qemu or Virtalbox/ware!")
+time.sleep(0.2)  
+print("Importing time")
+time.sleep(0.2)  
+print("importing os")
+time.sleep(0.2)  
+print("importing math")
+input("Press Enter to exit...")
